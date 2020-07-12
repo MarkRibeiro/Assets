12e4b830c72d08031b0ad90674941a45850b7ec8
mlhomsi <mlhomsi@gmail.com>
2020-07-12 04:59:08 -0300

    Jogo base

---
+++ Vanish.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vanish : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, 5);
+        Collider2D collider = this.GetComponent<Collider2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+}
