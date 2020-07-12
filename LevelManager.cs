78955829f94ee7a5cb71a413e7ec06847714fae3
mlhomsi <mlhomsi@gmail.com>
2020-07-12 13:36:34 -0300

    Navegacao

---
+++ LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void credits()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void startgame ()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
