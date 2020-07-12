0fa948977e1d0ce0ecefe98948f4368b05dd9a18
raqueru <raquelolho@hotmail.com>
2020-07-12 13:46:45 -0300

    Ajeuteu o personagem que segue o player pra ter a imagem

--- Scripts/EnemyTracker.cs
+++ Scripts/EnemyTracker.cs
@@ -11,19 +11,26 @@ public class EnemyTracker : MonoBehaviour
     public float MoveSpeed = 4;
     public float dano = 10;
 
+    private Animator animator;
+
+
+    private void Start()
+    {
+        animator = this.transform.GetChild(0).GetComponent<Animator>();
+        animator.SetBool("Andando",false);
+        animator.SetBool("Atacando",false);
+    }
+
 
     // Update is called once per frame
     void Update()
     {
+         
         if (Vector2.Distance(Player.transform.position, this.transform.position) < Miopia)
         {
-            transform.LookAt(Player.transform.position);
-            transform.eulerAngles = new Vector3(0f, 0f, transform.eulerAngles.z);
-
-            Vector3 direction = (Player.transform.position - this.transform.position).normalized;
-
-            transform.position += direction * MoveSpeed * Time.deltaTime;
+            walktowardsplayer();
         }
+        
     }
 
     void OnDrawGizmosSelected()
@@ -32,4 +39,36 @@ public class EnemyTracker : MonoBehaviour
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, Miopia);
     }
+
+
+    void walktowardsplayer()
+    {   
+        animator.SetBool("Andando",true);
+        transform.LookAt(Player.transform.position);
+        transform.eulerAngles = new Vector3(0f, 0f, transform.eulerAngles.z);
+
+        Vector3 direction = (Player.transform.position - this.transform.position).normalized;
+
+        transform.position += direction * MoveSpeed * Time.deltaTime;
+    }
+
+    
+
+     private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag=="Player"){
+            
+            animator.SetBool("Andando",false);
+            animator.SetBool("Atacando",true);
+        }
+
+
+    }
+
+     private void OnCollisionExit2D(Collision2D other) {
+        if (other.gameObject.tag=="Player"){
+             
+            animator.SetBool("Andando",true);
+            animator.SetBool("Atacando",false);
+        }
+     }
 }
