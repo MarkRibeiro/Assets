cbd46497ebcb69229a386c6ff80c7a655e44499d
raqueru <raquelolho@hotmail.com>
2020-07-12 14:17:32 -0300

    mudanca de codigos e prefab do golem

--- Scripts/Tiro/EnemyShoot.cs
+++ Scripts/Tiro/EnemyShoot.cs
@@ -8,11 +8,22 @@ public class EnemyShoot : ShootAttack
     public GameObject Player;
     public float Miopia = 100f;
 
+
+    private void Start() {
+        Player= GameObject.FindGameObjectWithTag("Player");
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Vector2.Distance(Player.transform.position, this.transform.position) < Miopia)
         {
+            if(Player.transform.position.x<this.transform.position.x){
+            transform.rotation= new Quaternion(0,180,0,0);
+        }
+        else transform.rotation= new Quaternion(0,0,0,0);
+
+       
             Vector2 direction = (Player.transform.position - this.transform.position).normalized;
             Shoot(direction);
         }
